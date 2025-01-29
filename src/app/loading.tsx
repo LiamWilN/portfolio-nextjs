8b16375fae62294beb9190bb869907e08a56c1b6
William C <CaldaWilliamN@gmail.com>
2025-01-29 23:24:18 +0800

    Added Spinners

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,23 @@
+import { CSSProperties } from "react";
+import { HashLoader } from "react-spinners";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
+
 export default function LoadingPage() {
-  return <div>Loading ...</div>;
+  return (
+    <div className="w-full h-full flex items-center justify-center mx-auto">
+      <HashLoader
+        color="text-neutral-800"
+        loading={true}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  );
 }
