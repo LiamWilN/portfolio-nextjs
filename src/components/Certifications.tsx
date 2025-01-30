775d08a5574e4339e1a84df849e634a5a6442760
William C <CaldaWilliamN@gmail.com>
2025-01-30 10:42:53 +0800

    Update Certifications

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -2,25 +2,28 @@ import data from "@/assets/me.json";
 import Image, { StaticImageData } from "next/image";
 import CSSCert from "@/assets/certifications/CSS Bootstrap and Javascript Certificate.jpg";
 import PowerCert from "@/assets/certifications/Fundamental Course of Microsoft Power Platform.jpg";
+import ReactCert from "@/assets/certifications/React JS Crash Course.jpg";
 
 type CertificationProps = {
   "CSS Bootstrap and Javascript": StaticImageData;
   "Fundamental Course of Microsoft Power Platform": StaticImageData;
+  "React JS Course": StaticImageData;
 };
 
 export default function Certifications() {
   const certs: CertificationProps = {
     "CSS Bootstrap and Javascript": CSSCert,
     "Fundamental Course of Microsoft Power Platform": PowerCert,
+    "React JS Course": ReactCert,
   };
   return (
     <div className="mt-10 border-t-[1px] py-4">
       <h1 className="font-semibold text-2xl">Certifications</h1>
-      <ul className={`space-y-4" `}>
+      <ul className={`grid grid-cols-2 mt-2`}>
         {data.certifications.map((item) => (
           <li
             data-aos="fade-right"
-            className="p-2 space-y-2 hover:bg-neutral-800 hover:dark:bg-neutral-200 rounded-tl-lg rounded-br-lg"
+            className="p-2 hover:bg-neutral-800 hover:dark:bg-neutral-200 rounded-tl-lg rounded-br-lg"
             key={item.id}
           >
             <a href={item.link} target="_blank">
