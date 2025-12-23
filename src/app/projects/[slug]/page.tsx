23ae9b15f182507915d8259df0ce96298aef3bac
LiamWilN <caldawilliamn@gmail.com>
2025-12-23 13:11:03 +0800

    Add Line Clamp for Esthetix. Hahaha

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -90,10 +90,10 @@ export default async function Project({ params }: { params: Params }) {
       </section>
       <br />
       <section className="p-2 pt-4">
-        <p className="text-[10px] italic">
+        <p className="text-[10px] italic line-clamp-2">
           *** Important Note: To protect project confidentiality and security, full project visuals are not publicly displayed.
-          Sharing complete images may expose sensitive details related to project anonymity, functionality, and internal processes. 
-          Only carefully selected snippets are shown to demonstrate credibility while maintaining compliance and protection. ***        </p>
+          Complete images may expose sensitive details related to project anonymity, functionality, and internal processes. 
+          Only carefully selected snippets are shown to demonstrate credibility while maintaining compliance and protection. ***
         </p>
       </section>
     </section>
